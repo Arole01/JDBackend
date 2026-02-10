ce41d34f804d333371154dd361ab45f9873c9d07
Arole01 <ajosedavidayobami@gmail.com>
2026-02-10 18:34:21 +0000

    error message

--- controllers/appController.js
+++ controllers/appController.js
@@ -39,6 +39,14 @@ exports.createUser = async (req,res)=>{
         })
     }
 
+    const userExist = await userModel.findOne({email: email.trim().toLowerCase()})
+
+    if(userExist){
+        return res.status(400).json({
+            message: `User with email ${email} already exist`
+        })
+    }
+
         const saltPassword = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, saltPassword)
         const userinfo = {
@@ -50,10 +58,16 @@ exports.createUser = async (req,res)=>{
 
 
         const user = await userModel.create(userinfo)
-        res.json(user)
+        res.status(200).json({
+            message: `User created successfully`,
+            data: user
+        })
 
 
     } catch (error){
-        res.json(error.message)
+        res.status(500).json({
+            message: `Unable to create user`, 
+            error: error.message
+        })
     }
 }
