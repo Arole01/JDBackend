6abc3de584ca81eb1d8c234dd55bc42b53739f61
Arole01 <ajosedavidayobami@gmail.com>
2026-03-09 21:06:55 +0000

    createCategory

---
+++ controllers/categoryController.js
@@ -0,0 +1,22 @@
+const categoryModel = require('../models/category');
+
+exports.createCategory = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+
+        if (!name) {
+            return res.status(400).json({message: "Category name is required"})
+        }
+
+        const existing = await categoryModel.findOne({ name: name.trim().toLowerCase() });
+        if (existing) {
+            return res.status(400).json({message: "Category already exists"})
+        }
+        const category = await categoryModel.create({
+            name, description
+        })
+        res.status(201).json({message: "Category created successfully", data: category})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
